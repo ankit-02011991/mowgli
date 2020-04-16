b22625b299f83a9444f2771ae6505bc3d634c3af
ankit-trivedi <ankit@pooraa.com>
2020-04-16 20:28:31 +0530

    add whole maven project

---
+++ pooraa/src/main/java/com/pooraa/merapooraa/module2.java
@@ -0,0 +1,13 @@
+package com.pooraa.merapooraa;
+
+
+import org.testng.annotations.Test;  
+  
+public class module2   
+{  
+    @Test  
+  public void test3()  
+  {  
+      System.out.println("hindi100.com");  
+  }  
+}  
